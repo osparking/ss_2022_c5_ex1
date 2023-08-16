29e1d657fbd22ca435520db422cb1b32ea6b5802
osparking <jbpark03@gmail.com>
2023-08-16 23:06:02 +0900

    initial

---
+++ src/main/java/space/bum/Ss2022C5Ex1Application.java
@@ -0,0 +1,13 @@
+package space.bum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2022C5Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss2022C5Ex1Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/Ss2022C5Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2022C5Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
