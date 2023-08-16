2a94ef3ee7734cb2a01960e73369ae313c7bf1ca
osparking <jbpark03@gmail.com>
2023-08-16 23:58:11 +0900

    password encoder bean method

--- src/main/java/space/bum/c5_ex1/config/SecurityConfig.java
+++ src/main/java/space/bum/c5_ex1/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,9 +22,15 @@ public class SecurityConfig {
 	@Bean
 	UserDetailsService userDetailsService() {
 		var uds = new InMemoryUserDetailsManager();
-		var u1 = User.withUsername("park").password("1234").authorities("read").build();
+		var u1 = User.withUsername("park")
+				.password("1234").authorities("read").build();
 		
 		uds.createUser(u1);
 		return uds;
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); 
 	}
 }
